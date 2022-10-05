8f03f36dce66b7fdac7a2cd26bb9579a96a7419e
Robert <robert.salmi@hiq.fi>
2022-10-05 10:50:57 +0300

    Merged feature-add-exercise to dev

--- main.js
+++ main.js
@@ -1,1 +1,46 @@
-console.log("Hello!")
+
+
+let exerciseList = [];
+
+
+
+function addExercise() {
+    
+    if (document.getElementById('exercises').value != '') {
+        // Empty ul and errorMessage
+        document.getElementById('exerciseList').innerHTML = '';
+        document.getElementById('messageContainer').innerHTML = '';
+        
+
+        // Get the exercise name from input field
+        const exerciseInput = document.getElementById('exercises');
+        console.log(exerciseInput.value);
+        
+        // Add exercise to array
+        exerciseList.push(exerciseInput.value);
+        console.log(exerciseList);
+
+        // Export items froms array and add items as li element to exerciseList
+        exerciseList.map((exercise) => {
+            const listElement = document.createElement('li');
+            listElement.textContent = exercise;
+            document.getElementById('exerciseList').appendChild(listElement);
+        });
+
+        // Empty input field
+        exerciseInput.value = '';
+
+    } else {
+
+        // Empty error message
+        document.getElementById('messageContainer').innerHTML = '';
+        
+        // Create new p element for error message and add it to errorMessageContainer
+        const errorMessage = document.createElement('p');
+        errorMessage.textContent = 'You need to enter exercise!';
+        document.getElementById('messageContainer').appendChild(errorMessage);
+
+    }
+}
+
+
