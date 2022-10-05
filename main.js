882002e1a3ccc7e0273a6c18a4ac0b624ae7047e
Robert <robert.salmi@hiq.fi>
2022-10-05 14:24:17 +0300

    Added message if user deletes last exercise from list. Also removed console logs.

--- main.js
+++ main.js
@@ -4,30 +4,29 @@ let exerciseList = [];
 
 
 function exercisesToList() {
+
     // Export items froms array and add items as li element to exerciseList
     exerciseList.map((exercise) => {
     
+        // Variables for li element
         const listElement = document.createElement('li');
+
+        // Variable for p element. Inserts exercise name to p element
         const exerciseText = document.createElement('p');
         exerciseText.textContent = exercise;
         
-        // Delete button
+        // Add delete button
         const btn = document.createElement('button');
         btn.innerHTML = 'Delete';
-
-        // Tämä ei toimi!!!
-        //btn.setAttribute("onclick","deleteExercise(e)");
-        
-        // Miksi tämä toimii???
-        //btn.onclick = function (e) {console.log(e.target)}
-
         btn.addEventListener('click', deleteExercise)
         
+        // Adds p element and button to li element and after that adds li element to ul
         listElement.appendChild(exerciseText);
         listElement.appendChild(btn);
         document.getElementById('exerciseList').appendChild(listElement);
 
     });
+
 }
 
 
@@ -36,22 +35,36 @@ function deleteExercise(e) {
     // Empty ul
     document.getElementById('exerciseList').innerHTML = '';
 
+    // Checks the name of the exercise which is being deleted
     const targetLi = e.target.previousSibling.innerHTML;
-    console.log(`Exercise ${targetLi} deleted!`);
     
+    // Checks array index of the exercise which is being deleted
     const indexOfTarget = exerciseList.indexOf(targetLi);
-    console.log(indexOfTarget);
 
+    // Use array index to specify which exercise is going to be deleted
     exerciseList.splice(indexOfTarget, 1);
 
     // Export items froms array and add items as li element to exerciseList
     exercisesToList();
+
+    // If there's no exercises after deleting exercise show message to user
+    if (exerciseList.length <= 0) {
+
+        // Creates p element for message and add some text into element
+        const messageExerciseListEmpty = document.createElement('p');
+        messageExerciseListEmpty.textContent = `You removed all list elements. Add new exercises.`;
+
+        // Adds p element to ul
+        document.getElementById('exerciseList').appendChild(messageExerciseListEmpty);
+    }
     
 }
 
 function addExercise() {
     
+    // Check is the input field filled
     if (document.getElementById('exercises').value != '') {
+
         // Empty ul and errorMessage
         document.getElementById('exerciseList').innerHTML = '';
         document.getElementById('messageContainer').innerHTML = '';
@@ -59,11 +72,9 @@ function addExercise() {
 
         // Get the exercise name from input field
         const exerciseInput = document.getElementById('exercises');
-        console.log(exerciseInput.value);
         
         // Add exercise to array
         exerciseList.push(exerciseInput.value);
-        console.log(exerciseList);
 
         // Export items froms array and add items as li element to exerciseList
         exercisesToList();
